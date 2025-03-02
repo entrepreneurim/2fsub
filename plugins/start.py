879971452479545515ad6f70014dbfdae471a735
entrepreneurim <iamakashrabha@gmail.com>
2025-03-02 21:44:26 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -131,7 +131,7 @@ REPLY_ERROR = "<code>Use this command as a reply to any Telegram message.</code>
 async def not_joined(client: Client, message: Message):
     buttons = [
         [
-         InlineKeyboardButton(text="ғᴏʟʟᴏᴡ ᴏɴ ɪɴsᴛᴀɢʀᴀᴍ", url="InlineKeyboardButton(text="https://www.instagram.com/animexgallery.ofc"),
+         InlineKeyboardButton(text="ғᴏʟʟᴏᴡ ᴏɴ ɪɴsᴛᴀɢʀᴀᴍ", url="https://www.instagram.com/animexgallery.ofc"),
         ],
         [
          InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
